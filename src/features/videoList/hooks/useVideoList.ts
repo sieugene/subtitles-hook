ae6220a01d26de62ebac32060af6daebd336ab17
sieugene <sieugene@mail.ru>
2024-11-18 22:37:17 +0900

    off session save

--- src/features/videoList/hooks/useVideoList.ts
+++ src/features/videoList/hooks/useVideoList.ts
@@ -2,55 +2,20 @@ import { useEffect, useState } from "react";
 
 import { MediaFile } from "../../../shared/types";
 
-const STORAGE_KEY = "file-list";
-
 export const useVideoList = () => {
   const [list, setList] = useState<MediaFile[]>([]);
-  const fetchLocal = useLocalFiles();
 
   useEffect(() => {
     (async () => {
-      try {
-        const data = await fetchLocal();
-        setList(data || []);
-      } finally {
-        sync();
-      }
+      const response = await fetch("data/files.json");
+      const data = (await response.json()) as MediaFile[];
+      setList(data || []);
     })();
   }, []);
 
-  const sync = () => {
-    setList((prevList) => {
-      const combinedList = [...prevList, ...getStorageData()].filter(
-        (d) => !!d
-      );
-      const uniqueList = Array.from(
-        new Map(combinedList.map((item) => [item.video, item])).values()
-      );
-      return uniqueList;
-    });
-  };
-
-  const getStorageData = () => {
-    return JSON.parse(
-      sessionStorage.getItem(STORAGE_KEY) || "[]"
-    ) as MediaFile[];
-  };
   const add = (media: MediaFile) => {
-    sessionStorage.setItem(
-      STORAGE_KEY,
-      JSON.stringify([...getStorageData(), media].filter((d) => !!d))
-    );
-    sync();
+    setList((prev) => [...prev, media]);
   };
 
   return { list, add };
-};
-
-const useLocalFiles = () => {
-  return async () => {
-    const response = await fetch("data/files.json");
-    const data = (await response.json()) as MediaFile[];
-    return data;
-  };
 };
